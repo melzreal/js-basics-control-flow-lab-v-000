54f2dfc789a696a72ae77dca8163dc15ec5f67c9
Melissa Real <melissa.real@hotmail.com>
2019-01-19 23:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-
+  let message;
     if (someValue <= 400) {
       return 'This one is on me!';
     } else (someValue > 2000) {
