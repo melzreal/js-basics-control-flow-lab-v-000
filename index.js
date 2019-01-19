a45d920f21fdb9cb14bb2d5ca46000dffba33f2a
Melissa Real <melissa.real@hotmail.com>
2019-01-19 23:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let charge;
+    if (feet <= 400) {
+      charge = 'This one is on me!';
+    } else (feet > 2000) {
+    charge = 'I will gladly take your thirty bucks.';
+    } else (feet > 2500 ) {
+
+     charge = 'No can do.';
+    }
+    return charge;
+  }
