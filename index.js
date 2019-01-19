7eded46ef724a6959d00e12f7161187e99e66083
Melissa Real <melissa.real@hotmail.com>
2019-01-19 23:17:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 function scuberGreetingForFeet(someValue) {
   let message;
     if (someValue <= 400) {
-      return 'This one is on me!';
-    } else (someValue > 2000) {
-    return 'I will gladly take your thirty bucks.';
-    }
+      message = 'This one is on me!';
+    } else if (someValue > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  } else {
+    message = 'No can do.'
+  }
   }
