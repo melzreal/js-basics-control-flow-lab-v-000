f4dc58c93c99d43f6ce589168432808f2f8b4ce7
Melissa Real <melissa.real@hotmail.com>
2019-01-19 23:31:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let message;
+    if (someValue <= 400) {
+      message = 'This one is on me!';
+    } else if ((someValue > 2000) && (someValue < 2500)) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if(someValue > 2500){
+    message = 'No can do.';
+  }
+   return message;
+}
+
+function ternaryCheckCity(city){
+  return city == 'NYC' ?  'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(goods){
+  let mess;
+  switch (goods) {
+    case 'generous':
+       mess =  'Thank you so much.';
+       break;
+    case 'not as generous':
+      mess = 'Thank you.';
+      break;
+    default: mess = 'Bye.';
+
+  }
+ return mess;
+}
